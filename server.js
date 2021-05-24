c4b2f7c0160f1ea5e9222077ac8a26f8fe4cb4bb
Shugi Yen Lazala <shugi.lazala@yahoo.com>
2021-05-24 14:53:22 +0900

    removed

--- server.js
+++ server.js
@@ -6,29 +6,18 @@ const cors = require('cors');
 const {readdirSync} = require('fs');
 require('dotenv').config();
 const connectDB = require('./config/db');
-const {ExpressPeerServer} = require('peer');
 // app
 const app = express();
 connectDB();
 // db
-const http = require('http').createServer(app);
 
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(cors());
 
-ExpressPeerServer(http, {path: '/'});
-
 // routes middleware
 readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 // port
 const port = process.env.PORT || 8000;
